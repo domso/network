6be52610c72b8a8785f8adc15866a61901e5e721
domso <domso94@googlemail.com>
2016-12-04 22:46:33 +0000

    add tcp_server-demo

---
+++ demo/tcp_server.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "network/tcp_socket.h"
+
+
+void server() {
+	
+	network::tcp_socket<network::ipv4_addr> socket;
+
+	network::ipv4_addr addr;
+
+	addr.init("", 80);
+
+	if(!socket.acceptOn(addr, 10)) {
+		std::cout << "Could not create tcp-socket" << std::endl;
+	}
+
+
+	while (true) {
+		network::tcp_connection<network::ipv4_addr>* connection = socket.acceptConnection();
+
+		
+
+		if (connection != nullptr) {
+			std::cout << "new connection!" << std::endl;
+
+			std::string message = "Hi!";
+			std::vector<char> buffer(message.begin(), message.end());
+
+			connection->sendData(buffer, buffer.size());
+
+			delete connection;
+		} else {
+			std::cout << "error with new connection! o0 " << std::endl;
+			return;
+
+		}
+	}
+
+	while (!socket.closeSocket()) {}
+
+}
+
+
+
+int main(int argc, char** argv) {
+    server();
+}
+
+
